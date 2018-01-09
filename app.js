14afc51a582b0f63fc670384c97244430bff736e
chanoch <email@chanoch.com>
2018-01-09 19:57:58 +0000

    Initial express template project for bodypoise.

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var helmet = require('helmet');
+var compression = require('compression');
+
+var ErrorHandler = require('express-error-handler');
+
+var index = require('./routes/index');
+
+var app = express();
+// app.use(compression);
+
+// TODO uncomment when you've created a favicon
+// app.use(favicon(path.join(__dirname, 'public', '/assets/img/bp_favicon.png')));
+app.use(logger('dev'));
+app.use(helmet());
+app.set('views', path.join(__dirname, '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  if(req.get('X-Forwarded-Proto') === 'http') {
+      res.redirect('https://' + req.get('Host') + req.url);
+  }
+  else
+      next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+var errorHandler = ErrorHandler({
+  static: {
+    '500': './public/error.html',
+    '404': './public/error.html',
+  }
+}) 
+
+app.use(errorHandler);
+
+module.exports = app;
---
+++ public/js/team-overlay.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+ 
+   $(document).ready(function(){
+        if (Modernizr.touch) {
+            // show the close overlay button
+            $(".close-overlay").removeClass("hidden");
+            // handle the adding of hover class when clicked
+            $(".team-img").click(function(e){
+                if (!$(this).hasClass("hover")) {
+                    $(this).addClass("hover");
+                }
+            });
+            // handle the closing of the overlay
+            $(".close-overlay").click(function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                if ($(this).closest(".team-img").hasClass("hover")) {
+                    $(this).closest(".team-img").removeClass("hover");
+                }
+            });
+        } else {
+            // handle the mouseenter functionality
+            $(".team-img").mouseenter(function(){
+                $(this).addClass("hover");
+            })
+            // handle the mouseleave functionality
+            .mouseleave(function(){
+                $(this).removeClass("hover");
+            });
+        }
+    });
