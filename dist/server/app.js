bceab3af62be93a962aad9fd83a4c45e6d8a788d
Chanoch <chanoch@clearbrook.it>
2018-02-05 12:40:56 +0000

    Fixed redirect issue http->https

--- dist/server/app.js
+++ dist/server/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(function(req, res, next) {
   if(req.get('X-Forwarded-Proto') === 'http') {
-      res.redirect('https://' + req.get('Host') + req.url);
+      res.redirect('https://bodypoise.co.uk' + req.url);
   }
   else
       next();
